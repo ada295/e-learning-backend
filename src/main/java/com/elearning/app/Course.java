c4088efc30377b2b3f9b02670cb3c0ff9aa697cb
Ada <adrianna.maria.zajac@gmail.com>
2023-01-24 19:26:34 +0100

    Added initial backend application

---
+++ src/main/java/com/elearning/app/Course.java
@@ -0,0 +1,46 @@
+package com.elearning.app;
+
+public class Course {
+    private Long id;
+    private String name;
+    private String description;
+
+    public Course (Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/elearning/app/CourseController.java
@@ -0,0 +1,29 @@
+package com.elearning.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> getCourses () {
+        List<Course> courses = new ArrayList<>();
+
+        courses.add(new Course(1L,"Matma", "Liczby rzeczywiste"));
+        courses.add(new Course(2L, "Biologia", "Człowiek"));
+        courses.add(new Course(3L, "IT", "Prolog"));
+
+        return courses;
+    }
+
+    @PostMapping("/courses")
+    public void saveCourses (@RequestBody List<Course> courses) {
+        System.out.println(courses);
+    }
+}
