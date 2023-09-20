ac90edeab19d20a9d5cb8c8ddf8b9d9fcfeaa9b4
Ada <adrianna.maria.zajac@gmail.com>
2023-09-20 22:35:58 +0200

    Added path to UserController

--- src/main/java/com/elearning/app/user/UserController.java
+++ src/main/java/com/elearning/app/user/UserController.java
@@ -1,6 +1,7 @@
 package com.elearning.app.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +9,38 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-public class StudentController {
+public class UserController {
 
     @Autowired
     private UserRepository repository;
 
-    @GetMapping("/users")
-    public List<UserAccount> getUsers() {
+    @GetMapping("/students")
+    public List<UserAccount> getStudents() {
         return repository.findAllByRoles(Collections.singletonList(UserRole.STUDENT));
     }
 
-    @GetMapping("/users/search")
+    @GetMapping("/admins")
+    public List<UserAccount> getAdmins() {
+        return repository.findAllByRoles(Collections.singletonList(UserRole.ADMIN));
+    }
+
+    @GetMapping("/teachers")
+    public List<UserAccount> getTeachers() {
+        return repository.findAllByRoles(Collections.singletonList(UserRole.TEACHER));
+    }
+
+    @GetMapping("/users")
+    public List<UserAccount> getAll() {
+        List<UserAccount> users = getStudents();
+        users.addAll(getAdmins());
+        users.addAll(getTeachers());
+
+        return users;
+    }
+
+    @GetMapping("/students/search")
     public List<UserAccount> searchUsers(@RequestParam String name, @RequestParam String surname) {
         if ((name == null || "".equals(name)) && (surname == null || "".equals(surname))) {
             //zwracamy wszystkich uzytkownikow
